9a191439b3f727a285011a71b3a245a47339999d
Stotts, Greg <gstotts@hy-vee.com>
2021-06-04 15:54:12 -0500

    Structured data

---
+++ 01 - go fundamentals/01 - syntax-examples/08_structured-data.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Foo struct {
+	Bar string
+	Baz string
+}
+
+func main() {
+	f := Foo{"Joe Junior", "Hello Shabado"}
+	b, _ := json.Marshal(f)
+	fmt.Println(string(b))
+	json.Unmarshal(b, &f)
+}
